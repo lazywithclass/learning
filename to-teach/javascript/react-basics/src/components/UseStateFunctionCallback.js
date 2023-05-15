999f10352bf40c2f2f2ab0c0312e0e697b6e1ffc
Alberto Zaccagni <montecristoh@gmail.com>
2023-05-15 10:54:10 +0200

    roba

--- to-teach/javascript/react-basics/src/components/UseStateFunctionCallback.js
+++ to-teach/javascript/react-basics/src/components/UseStateFunctionCallback.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 
+
 export function UseStateFunctionCallback() {
   const [content, setContent] = useState('')
   const [sum, setSum] = useState(0)
   const [ok, setOk] = useState(false)
+  const [clickedCount, setClickedCount] = useState(0)
 
   function onChangeMadness(e) {
     setContent(previousContent =>
@@ -39,6 +41,24 @@ export function UseStateFunctionCallback() {
         value={ok}
         onChange={onChangeCheckbox}
       />
+      <br />
+      <br />
+      {clickedCount}
+      <ChildComponent setClickedCount={setClickedCount} />
     </div>
+  )
+}
+
+function ChildComponent({ setClickedCount }) {
+  // in this scenario we don't have access to clickedCount here
+  // so we use the callback version of the set state function
+  function callback(count) {
+    return count + 1
+  }
+
+  return (
+    <button
+      onClick={() => setClickedCount(callback)}
+    >Click me</button>
   )
 }
