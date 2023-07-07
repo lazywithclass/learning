aad8ab4a07d29fab4d7a0d2d726f33ce8eb7b04f
Alberto Zaccagni <montecristoh@gmail.com>
2023-07-07 23:54:54 +0200

    added nixos

---
+++ to-teach/javascript/default.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  lib = import <nixpkgs/lib>;
+  buildNodeJs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> {
+    python = pkgs.python3;
+  };
+
+  nodejsVersion = lib.fileContents ~/.nvmrc;
+
+  nodejs = buildNodeJs {
+    enableNpm = true;
+    version = nodejsVersion;
+    sha256 = "g+AzgeJx8aVhkYjnrqnYXZt+EvW+KijOt41ySe0it/E=";
+  };
+
+  NPM_CONFIG_PREFIX = toString ./npm_config_prefix;
+
+in pkgs.mkShell {
+  packages = with pkgs; [
+    nodejs
+    nodePackages.npm
+  ];
+
+  inherit NPM_CONFIG_PREFIX;
+
+  shellHook = ''
+    export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
+  '';
+}
