e5e12898da53a6c5547e1ef13f4602cd68413696
Alberto Zaccagni <montecristoh@gmail.com>
2024-02-07 23:44:26 +0000

    first steps in melange

---
+++ parser-combinators/melange/README.md
@@ -0,0 +1,7 @@
+# Melange
+
+![melange](img/melange.jpeg)
+
+[https://fictionhorizon.com/the-meaning-of-dunes-the-spice-must-flow-quote/](The parsing must flow)
+
+Melange is a parser combinator library written in OCaml, it takes inspiration from [ https://swlaschin.gitbooks.io/fsharpforfunandprofit/content/posts/understanding-parser-combinators.html](the wonderful series on Parser Combinators by Scott Wlaschin) and from `scala.util.parsing.combinator._`.
---
+++ parser-combinators/melange/default.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  lib = import <nixpkgs/lib>;
+  buildNodeJs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> {
+    python = pkgs.python3;
+  };
+
+  nodejsVersion = lib.fileContents ~/.nvmrc;
+
+  nodejs = buildNodeJs {
+    enableNpm = true;
+    version = "18.17.1";
+    sha256 = "8hXPA9DwDwesC2dMaBn4BMFULhbxUtoEmAAirsz15lo=";
+  };
+
+in pkgs.mkShell {
+  packages = with pkgs; [
+    ocaml
+    ocamlPackages.ocaml-lsp
+    ocamlPackages.ocp-indent
+    dune_3
+    python3
+    deno
+    nodejs
+    nodePackages.npm
+  ];
+
+  # inherit NPM_CONFIG_PREFIX;
+
+  # shellHook = ''
+    # export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
+  # '';
+}
