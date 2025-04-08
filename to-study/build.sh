4608f857bac56099dd5ee293a5ce38b2890cee84
Alberto Zaccagni <montecristoh@gmail.com>
2025-04-08 15:25:04 +0200

    studying and spark

--- to-study/build.sh
+++ to-study/build.sh
@@ -4,11 +4,21 @@ OBSIDIAN_VAULT="obsidian-vault"
 NOTES_DIR="site"
 OUTPUT_FILE="$NOTES_DIR/notes.html"
 
+normalize_filename() {
+    local filename="$1"
+    echo "$filename" | tr '[:upper:]' '[:lower:]' | tr ' ' '-'
+}
 
 find $OBSIDIAN_VAULT/*md -maxdepth 1 -type f | while read -r file; do
-    echo "$file"
     filename=$(echo "$file" | cut -d/ -f2)
-    pandoc --quiet "$file" -o "$NOTES_DIR/$(echo $filename).html" --lua-filter=pandoc-lua/add_links.lua -H extra-head.html -A after-body.html
+    title=$(echo "$filename" | cut -d. -f1)
+    filename=$(normalize_filename "$filename")
+    if ! grep -q "$filename" whitelist; then
+      continue
+    fi
+    echo "$file"
+    pandoc --quiet "$file" --metadata title="$title" --template=custom.html5 -o "$NOTES_DIR/$(echo $filename).html" --lua-filter=pandoc-lua/add_links.lua
+
 done
 
 cat > "$OUTPUT_FILE" <<EOF
@@ -18,6 +28,9 @@ EOF
 
 for file in "$NOTES_DIR"/*; do
   filename=$(basename "$file")
+  if ! grep -q "$filename" whitelist; then
+    continue
+  fi
   cat >> "$OUTPUT_FILE" <<EOF
     <li><a href="$NOTES/$filename">"${filename%.html}"</a></li>
 EOF
@@ -30,3 +43,6 @@ EOF
 
 mkdir -p "$NOTES_DIR/attachments"
 cp -r "$OBSIDIAN_VAULT/attachments" "$NOTES_DIR/"
+
+mkdir -p "$NOTES_DIR/iframes"
+cp -r "iframes" "$NOTES_DIR/"
--- to-study/spark/test.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.getOrCreate()
-
-spark.sql("CREATE NAMESPACE IF NOT EXISTS s3tablesbucket.movies")
-spark.sql("CREATE TABLE IF NOT EXISTS s3tablesbucket.movies.lord_of_the_rings")
-
-df = spark.read.parquet("/home/spark/work/data/test_data.parquet")
-df.writeTo("s3tablesbucket.movies.lord_of_the_rings").createOrReplace()
