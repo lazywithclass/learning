c719c8c0ff3815666a201bf95f71aeb5b95de2d9
Alberto Zaccagni <montecristoh@gmail.com>
2025-04-15 20:58:26 +0200

    bigger dataset

--- postgresql/scripts/reset.sh
+++ postgresql/scripts/reset.sh
@@ -5,4 +5,4 @@ DB_NAME=imdb
 DB_USER=lazywithclass
 
 psql -U $DB_USER -d $DB_NAME -c "DROP SCHEMA imdb CASCADE;" -h $PGDATA
-psql -U $DB_USER -d $DB_NAME -f imdb_small.sql -h $PGDATA
+psql -U $DB_USER -d $DB_NAME -f imdb.sql -h $PGDATA
