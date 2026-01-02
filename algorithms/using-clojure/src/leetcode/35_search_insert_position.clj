dba140062cfda727548e5fd8bda4246c961a2539
Alberto Zaccagni <montecristoh@gmail.com>
2026-01-02 22:53:31 +0100

    leetcode 35

--- algorithms/using-clojure/src/leetcode/35_search_insert_position.clj
+++ algorithms/using-clojure/src/leetcode/35_search_insert_position.clj
@@ -1,4 +1,5 @@
-(ns leetcode.35-search-insert-position)
+(ns leetcode.35-search-insert-position
+  (:import [clojure.lang PersistentVector]))
 
 
 ;; Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
@@ -6,23 +7,23 @@
 ;; You must write an algorithm with O(log n) runtime complexity.
 
 
-(defn find-position [arr n]
+(defn search-insert [^PersistentVector arr ^long tgt]
   (loop [lo 0
          hi (dec (count arr))]
     (let [piv (quot (+ lo hi) 2)
           cur (nth arr piv)]
-    (cond
-      (> lo hi) lo
-      (= cur n) piv
-      (< cur n) (recur (inc piv) hi)
-      (> cur n) (recur lo (dec piv))))))
+      (cond
+        (> lo hi)   lo
+        (= cur tgt) piv
+        (< cur tgt) (recur (inc piv) hi)
+        (> cur tgt) (recur lo (dec piv))))))
 
-(find-position [1 2 3 4 5 6] 5) ;; 4
+(search-insert [1 2 3 4 5 6] 5) ;; 4
 
-(find-position [1 3 4 5 6] 2) ;; 1
+(search-insert [1 3 4 5 6] 2) ;; 1
 
-(find-position [1 2 3 4 6] 5) ;; 4
+(search-insert [1 2 3 4 6] 5) ;; 4
 
-(find-position [1 2 3 4 5] 42) ;; 5
+(search-insert [1 2 3 4 5] 42) ;; 5
 
-(find-position [1 2 3 4 5] 0) ;; 0
+(search-insert [1 2 3 4 5] 0) ;; 0
