91e91c046b6fa5efaaeee64ba2ac491690041293
Alberto Zaccagni <montecristoh@gmail.com>
2026-01-07 14:56:06 +0100

    leetcode 3

--- algorithms/using-clojure/src/leetcode/3_longest_substring_without_repeating_characters.clj
+++ algorithms/using-clojure/src/leetcode/3_longest_substring_without_repeating_characters.clj
@@ -4,6 +4,9 @@
 
 ;; Given a sequence, find the length of the longest subsequence without duplicate characters.
 
+
+;; following solution uses too much space, O(n)
+;; I also mistakenly returned the whole longest subseq, instead just the len is required
 (defn longest-substr [^PersistentVector s]
   (loop [idx 0
          seen {}
@@ -20,6 +23,27 @@
 
         :else
         (recur (inc idx) (assoc seen element idx) (conj current element) longest)))))
+
+
+;; O(1) space
+(defn longest-substr [^PersistentVector s]
+  (loop [win-beg 0
+         win-end 0
+         seen {}
+         max-len 0]
+    (let [element (get s win-beg)]
+      (if (nil? element)
+        max-len
+        (let [prev-idx (get seen element)
+              new-end  (if (and prev-idx (>= prev-idx win-end))
+                         (inc prev-idx)
+                         win-end)
+              new-len  (- win-beg new-end -1)
+              new-max  (max max-len new-len)]
+          (recur (inc win-beg)
+                 new-end
+                 (assoc seen element win-beg)
+                 new-max))))))
 
 
 (longest-substr [\a \b \c \a \b \c \b \b]) ;; [\a \b \c]
